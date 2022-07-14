0fea0a8bc4755145ff68f3a64ee80e6bbf01e340
peter p <zhenwang429@gmail.com>
2022-07-15 02:52:25 +0800

    pracice

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style = "text-align: center">hello world</h1>'
+    line2 = '<img src = "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg?t=1650611880" width = 1500>'
+    return HttpResponse(line1+line2)
