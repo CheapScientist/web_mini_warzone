ef1d58e50dd2e4c021dfdf05304220c46bfe8e52
peter p <zhenwang429@gmail.com>
2022-07-15 06:22:52 +0800

    day2

--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style = "text-align: center">hello world</h1>'
-    line2 = '<img src = "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg?t=1650611880" width = 1500>'
-    return HttpResponse(line1+line2)
